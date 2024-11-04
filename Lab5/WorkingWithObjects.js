cf7bca5ba7389d7bb0a5670f60426b16955d1d25
agasti-mhatre <agastimhatre6@gmail.com>
2024-11-04 18:39:48 -0500

    Up to section 3.4

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -4,13 +4,61 @@ const assignment = {
     due: "2021-10-10", completed: false, score: 0,
 };
 
+const module = {
+
+    id: 2,
+    name: "React Basics",
+    description: "Basics of frontend programming",
+    course: "Full stack development"
+};
+
 export default function WorkingWithObjects(app) {
     
     app.get("/lab5/assignment", (req, res) => {
+        
         res.json(assignment);
     });
     
     app.get("/lab5/assignment/title", (req, res) => {
+
         res.json(assignment.title);
+    });
+
+    app.get("/lab5/assignment/title/:newTitle", (req, res) => {
+        
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    });
+
+    app.get("/lab5/module", (req, res) => {
+
+        res.json(module);
+    });
+
+    app.get("/lab5/module/name", (req, res) => {
+
+        res.json(module.name);
+    });
+
+    app.get("/lab5/module/:newName", (req, res) => {
+
+        const { newName } = req.params;
+        module.name = newName;
+        res.json(module);
+    });
+
+    app.get("/lab5/assignment/score/:newScore", (req, res) => {
+        
+        const { newScore } = req.params;
+        assignment.score = newScore;
+        res.json(assignment);
+    });
+
+    app.get("/lab5/assignment/status/:newStatus", (req, res) => {
+        
+        const { newStatus } = req.params;
+        assignment.completed = newStatus;
+        res.json(assignment);
     });
 };  
