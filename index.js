82439edd91dec60a7d61ed3f476b96e5a4d153ba
agasti-mhatre <agastimhatre6@gmail.com>
2024-11-03 23:48:34 -0500

    Up to section 3.3

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
+import QueryParameters from "./Lab5/QueryParameters.js";
 
 const app = express();
+
 Lab5(app);
 Hello(app);
+QueryParameters(app);
+
 app.listen(process.env.PORT || 4000);
