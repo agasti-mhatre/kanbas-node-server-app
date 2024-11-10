98b9e68a2640e2b92c71fc3fab796dc3f2e7e094
agasti-mhatre <agastimhatre6@gmail.com>
2024-11-10 00:06:21 -0500

    Up to section 4.4

--- index.js
+++ index.js
@@ -3,11 +3,41 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
 const app = express();
 
-app.use(cors());
+app.use(
+
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+);
+
+const sessionOptions = {
+    
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+    
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+  
 app.use(express.json());
+
+app.use(
+    session(sessionOptions)
+);
 
 UserRoutes(app);
 Lab5(app);
