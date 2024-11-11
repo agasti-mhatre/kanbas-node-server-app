352ad7d1d56250af95c6b4800c852b0bc89495fa
agasti-mhatre <agastimhatre6@gmail.com>
2024-11-11 18:26:39 -0500

    Up to 4.6

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -1,5 +1,13 @@
 import Database from "../Database/index.js";
 
+export function updateModule(moduleId, moduleUpdates) {
+
+    const { modules } = Database;
+    const module = modules.find((module) => module._id === moduleId);
+    Object.assign(module, moduleUpdates);
+    return module;
+}  
+
 export function deleteModule(moduleId) {
 
     const { modules } = Database;
