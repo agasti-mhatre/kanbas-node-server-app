e73a10ad9f67bd5c746154b00443efd9e0e9f4ed
agasti-mhatre <agastimhatre6@gmail.com>
2024-11-26 10:13:28 -0500

    Fixed issue. up to 3.6.5

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -8,12 +8,30 @@ export default function UserRoutes(app) {
   const deleteUser = (req, res) => { };
   
   const findAllUsers = async (req, res) => {
-    
+
+    const { role, name } = req.query;
+    if (role) {
+      
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
+
     const users = await dao.findAllUsers();
     res.json(users);
   };
   
-  const findUserById = (req, res) => { };
+  const findUserById = async (req, res) => {
+    
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
   
   const updateUser = (req, res) => {
     
