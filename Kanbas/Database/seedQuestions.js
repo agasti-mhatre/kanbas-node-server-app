f697d136f42d4f4800d22ab645d0ec4be9a9fe91
GracyMa <ma.tianyi@northeastern.edu>
2024-12-04 00:36:10 -0800

    update question.js to json format and update the logic in seedQuestion;

--- Kanbas/Database/seedQuestions.js
+++ Kanbas/Database/seedQuestions.js
@@ -2,6 +2,8 @@
 
 import mongoose from 'mongoose';
 import { QuestionModel } from '../Quizzes/model.js';
+import fs from 'fs/promises';
+import path from 'path';
 import questions from './question.js'
 
 
@@ -12,19 +14,27 @@ mongoose.connect(CONNECTION_STRING, {
   useUnifiedTopology: true,
 });
 
+const getQuestions = async () => {
+  const filePath = path.resolve('./questions.json');
+  const data = await fs.readFile(filePath, 'utf8');
+  return JSON.parse(data);
+}
+
 async function seedQuestions() {
   try {
     // Optional: Clear existing questions
     await QuestionModel.deleteMany({});
+    console.log('Existing data deleted!');
 
     // Insert questions into the database
+    const questions = await getQuestions();
     await QuestionModel.insertMany(questions);
-
     console.log('Questions seeded successfully');
   } catch (error) {
     console.error('Error seeding questions:', error);
   } finally {
     mongoose.disconnect();
+    console.log('DB disconnected');
   }
 }
 
